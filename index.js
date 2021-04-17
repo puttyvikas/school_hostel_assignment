9e0fb98d4744d244c512b01d66fab767e7957635
puttyvikas <putty.vikas@meesho.com>
2021-04-17 13:00:51 +0530

    validate the input

--- index.js
+++ index.js
@@ -2,13 +2,31 @@
 const prompt = require("prompt-async");
  
 async function schoolHostelAssignment() {
-    prompt.start();
- 
-    let { n } = await prompt.get(["n"]); // get the number of students
+  prompt.start();
+
+  let { n } = await prompt.get(["n"]); // get the number of students
+  const students = [];
+  for(let i=0; i<n; i++) {
     const result = await prompt.get(["id", "class", "food"]); // get id, class and food from input
-    console.log(n, result);
     
+    const errors = checkErrors(result);
+    if(errors.length !== 0) {
+      errors.forEach(error => console.log(error));
+      n++;
+      continue;
+    }
+  }
 }
- 
+
 schoolHostelAssignment();
+
+// check for valid input
+function checkErrors(obj) {
+  let errors = [];
+  if(obj.id.toString().length !== 4) errors.push('id must be 4 digits');
+  if(obj.class !== 'A' && obj.class !== 'B') errors.push('invalid class');
+  if(obj.food !== 'V' && obj.food !== 'NV') errors.push('invalid food preference');
+  return errors;
+}
+
 
